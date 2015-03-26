446b598a98ab6709cc1945689f8d6ac325f37d90
Reznoir <wuala.dtfv@gmail.com>
2015-03-26 12:26:23 +0100

    new object classes + getter and setter

--- Seene-Club-Toolkit/src/org/seeneclub/toolkit/SceneObject.java
+++ Seene-Club-Toolkit/src/org/seeneclub/toolkit/SceneObject.java
@@ -1,24 +1,25 @@
 package org.seeneclub.toolkit;
 
 import java.io.File;
-import java.util.*;
+import java.util.Date;
+import java.util.UUID;
 
 public class SceneObject {
 	/// header
-	public String caption;
-	public Date captured_at;
-	public String filter_code;
-	public int flash_level;
-	public UUID identifier;
-	public int orientation;
-	public int shared;
-	public int storage_version;
+	private String caption;
+	private Date captured_at;
+	private String filter_code;
+	private int flash_level;
+	private UUID identifier;
+	private int orientation;
+	private int shared;
+	private int storage_version;
 	
 	/// scene.oemodel
-	public File model;
+	private SeeneModel model;
 	
 	/// poster.jpg
-	public File poster;
+	private SeeneTexture poster;
 
 	SceneObject() {
 		caption = "#synthetic (Uploaded by https://github.com/seene-club/seene-toolkit by @sclub)";
@@ -29,6 +30,102 @@ public class SceneObject {
 		orientation = 0;
 		shared = 0;
 		storage_version = 3;
+	}
+	
+	// Getter and Setter
+	public File getModelFile() {
+		return model.getModelFile();
+	}
+	
+	public void setModelFile(File modelFile) {
+		model.setModelFile(modelFile);
+	}
+	
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public Date getCaptured_at() {
+		return captured_at;
+	}
+
+	public void setCaptured_at(Date captured_at) {
+		this.captured_at = captured_at;
+	}
+
+	public String getFilter_code() {
+		return filter_code;
+	}
+
+	public void setFilter_code(String filter_code) {
+		this.filter_code = filter_code;
+	}
+
+	public int getFlash_level() {
+		return flash_level;
+	}
+
+	public void setFlash_level(int flash_level) {
+		this.flash_level = flash_level;
+	}
+
+	public UUID getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(UUID identifier) {
+		this.identifier = identifier;
+	}
+
+	public int getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(int orientation) {
+		this.orientation = orientation;
+	}
+
+	public int getShared() {
+		return shared;
+	}
+
+	public void setShared(int shared) {
+		this.shared = shared;
+	}
+
+	public int getStorage_version() {
+		return storage_version;
+	}
+
+	public void setStorage_version(int storage_version) {
+		this.storage_version = storage_version;
+	}
+
+	public SeeneModel getModel() {
+		return model;
+	}
+
+	public void setModel(SeeneModel model) {
+		this.model = model;
+	}
+
+	public SeeneTexture getPoster() {
+		return poster;
+	}
+
+	public void setPoster(SeeneTexture poster) {
+		this.poster = poster;
+	}
+
+	public File getPosterFile() {
+		return poster.getTextureFile();
+	}
+	public void setTextureFile(File textureFile) {
+		poster.setTextureFile(textureFile);
 	}
 
 }
---
+++ Seene-Club-Toolkit/src/org/seeneclub/toolkit/SeeneTexture.java
@@ -0,0 +1,27 @@
+package org.seeneclub.toolkit;
+
+import java.awt.Image;
+import java.io.File;
+
+public class SeeneTexture {
+	
+	private Image textureImage;
+	private File textureFile;
+	
+	
+	// Getters and Setters
+	public Image getTextureImage() {
+		return textureImage;
+	}
+	public void setTextureImage(Image textureImage) {
+		this.textureImage = textureImage;
+	}
+	public File getTextureFile() {
+		return textureFile;
+	}
+	public void setTextureFile(File textureFile) {
+		this.textureFile = textureFile;
+	}
+	
+
+}
